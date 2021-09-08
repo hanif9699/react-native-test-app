7e91173905e0bc08cfb0e950d853d553fc34a88c
Badsha Hanif <badsha@paperplane.net>
2021-09-08 21:21:04 +0530

    'procfile changes'

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
+  console.log(config)
   // Customize the config before returning it.
   config.output.publicPath = '/';
   config.devServer = {
